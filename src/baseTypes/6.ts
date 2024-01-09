ab4658937407cdd3501d2e2bdc2d30af0db7d39b
Sergey Kuran <kuran.serg@gmail.com>
2024-01-09 21:36:26 +0200

    copleted task 7

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,15 +2,21 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
-  console.log(message);
+function showMessage(message: string): void {
+  if (typeof message === 'string') console.log(message)
+  
+  console.log('No show this message, becouse is type "number"')
+  
 }
 
-function calc(num1, num2) {
-  return num1 + num2;
+function calc(num1: string | number, num2: string | number): string | number {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2
+  }
+  return num1.toString()+num2.toString()
 }
 
-function customError() {
+function customError(): never {
   throw new Error('Error');
 }
 
