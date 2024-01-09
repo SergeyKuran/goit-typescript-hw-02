962d0df3bad37f138d662fb0c8e640822a17e61e
Sergey Kuran <kuran.serg@gmail.com>
2024-01-09 21:16:55 +0200

    completed task 4

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -2,7 +2,8 @@
   Як переписати його в TypeScript, використовуючи концепцію кортежів, 
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
-type personTuple=[string,number]
+
+type personTuple = [string, number]
 let person: personTuple = ['Max', 21];
 
-export {};
+export { };
