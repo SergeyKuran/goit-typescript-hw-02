1a8cd758019ee66659b9005d8d2c7419cc3c7baa
Sergey Kuran <kuran.serg@gmail.com>
2024-01-23 22:06:00 +0200

    completed task 4 generic type

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,24 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface ComponentPage {
+  title: string
+}
+
+class Component<T extends ComponentPage> {
+  constructor(public props: T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class PageProps implements ComponentPage {
+  title: string = '';
+}
+
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+export { };
